d225c4f063474c63ae9add3241411fd79e0c2ad3
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2023-12-06 20:44:28 +0300

    a lot of tasks

---
+++ 2022/task4/src/lib.rs
@@ -0,0 +1,11 @@
+use std::fs;
+
+pub fn read_input() -> Vec<String> {
+    let content = fs::read_to_string("./input.txt").unwrap();
+    let lines = content
+        .split("\n")
+        .map(|line| line.trim().to_string())
+        .collect::<Vec<String>>();
+
+    return lines;
+}
